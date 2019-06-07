a149364675179d769a849d89cadc4fdba4b6aa1f
VoidChips <voidchips@gmail.com>
2019-06-07 00:41:48 -0500

    change page section and highlight

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
-// import Buy from './components/Buy/Buy';
-// import Sell from './components/Sell/Sell';
+import Buy from './components/Buy/Buy';
+import Sell from './components/Sell/Sell';
 import About from './components/About/About';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {page_section: 'about'}
+  constructor(props) {
+    super(props);
+    this.state = {
+      page_section: 'about',
+      current_page_class: 'unselected',
+      isSignedin: false
+    }
+  }
+
+  // updates what part of the website the user is in
+  updateSection = (section) => {
+    this.setState({ page_section: section });
+    this.setState({ current_page_class: 'selected' });
+  }
+
+  signIn = () => {
+    this.setState({ isSignedin: true });
   }
 
   render() {
-    const {page_section} = this.state;
+    const { page_section, current_page_class, isSignedin } = this.state;
+
+    // change section based on what page_section is
+    const changeSection = () => {
+      switch (page_section) {
+        case 'about':
+          return <About />
+        case 'buy':
+          return <Buy />
+        case 'sell':
+          return <Sell />
+        default:
+          return <h2>Loading...</h2>
+      }
+    }
     return (
       <div className="App">
         <h1>Fakeconomy</h1>
-        <Navbar />
-        {page_section === 'about' ? <About />
-        : <h2>Loading...</h2>
-        }
+        <Navbar updateSection={this.updateSection} page_section={page_section} current_page_class={current_page_class} signIn={this.signIn} isSignedin={isSignedin} />
+        {changeSection()}
+
+
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,19 +3,49 @@ import React from 'react';
 // import Register from './Register/Register';
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ updateSection, page_section, current_page_class, signIn, isSignedin }) => {
+
+    const selectCSS = (page) => {
+        if (page === page_section) {
+            return current_page_class;
+        }
+        else {
+            return 'unselected';
+        }
+    }
+
     return (
         <nav id='navbar'>
             <ul id='main-content'>
-                <li>Buy</li>
-                <li>Sell</li>
-                <li>About</li>
+                <li
+                    
+                    onClick={() => {
+                        updateSection('buy')}
+                    }
+                    className={selectCSS('buy')}
+                >Buy</li>
+                <li
+                    
+                    onClick={() => updateSection('sell')}
+                    className={selectCSS('sell')}
+                >Sell</li>
+                <li
+                    
+                    onClick={() => updateSection('about')}
+                    className={selectCSS('about')}
+                >About</li>
+            </ul>
+            {
+                !isSignedin ?
+                    <ul id='login'>
+                        <li onClick={signIn}>Login</li>
+                        <li onClick={signIn}>Register</li>
+                    </ul>
+                    :
+                    <p>Signed in</p>
 
-            </ul>
-            <ul id='login'>
-                <li>Login</li>
-                <li>Register</li>
-            </ul>
+            }
+
         </nav>
     );
 }
